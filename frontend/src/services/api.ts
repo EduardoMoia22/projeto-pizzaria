8a42159d75fedd6805a2cfe8c27e2899e7b8eb2b
EduardoMoia22 <eduardomoiasantos@gmail.com>
2022-07-27 12:59:41 -0300

    Funcao signOut

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosError } from "../../node_modules/axios/index";
 import { parseCookies } from "../../node_modules/nookies/dist/index";
+import { signOut } from "../contexts/AuthContext";
 import { AuthTokenError } from "./errors/AuthTokenError";
 
 export function setupAPIClient(ctx = undefined){
@@ -18,7 +19,7 @@ export function setupAPIClient(ctx = undefined){
     if(error.response.status === 401){
       //Qualquer erro 401 (nao autorizado) devemos deslogar o usuario
       if(typeof window !== undefined){
-        //Chamar a funçao para deslogar o usuario
+        signOut()
       }else{
         return Promise.reject(new AuthTokenError())
       }
